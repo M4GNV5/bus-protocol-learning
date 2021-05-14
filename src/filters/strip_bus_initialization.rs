14a160e3ccc010a57daea441dd13058b4a064a3a
Jakob Löw <jakob@m4gnus.de>
2021-05-14 11:37:53 +0200

    prevent panics with StripInitialization

--- src/filters/strip_bus_initialization.rs
+++ src/filters/strip_bus_initialization.rs
@@ -1,4 +1,7 @@
+use std::cmp::min;
+
 pub fn strip_bus_initialization(values: &mut Vec<f64>, initialization_length: usize) -> f64 {
-	values.drain(0..initialization_length);
+	let drain_len = min(initialization_length, values.len() - 1);
+	values.drain(0..drain_len);
 	1f64
 }
